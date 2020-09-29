c18f858567309244b88ae1e8a7b151a2975d2e9e
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-29 11:07:10 +0300

    chaining middlewares

--- app.js
+++ app.js
@@ -1,54 +1,42 @@
-'use strict';
+"use strict";
 console.clear();
 global.log = console.log;
 
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-
 const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app
-    .listen(3000, log(`Server running on port ${PORT} \n`))
-
+app.listen(3000, log(`Server running on port ${PORT} \n`));
 
 /* ===================================
     MOUTE MIDLLEWARES
 ===================================== */
 
 /* Serve static assets */
-const statics = express.static(process.cwd() + '/public');
-app.use(statics)
+const statics = express.static(process.cwd() + "/public");
+app.use(statics);
 
+app.route("/").get((req, res) => {
+  // SERVE HTML FILES
+  res.sendFile(__dirname + "/index.html");
+});
 
-app
-    .route('/')
-    .get((req, res) => {
-        // SERVE HTML FILES
-        res.sendFile(__dirname + '/index.html')
-    })
+app.route("/now").get(
+  (req, res, next) => {
+    req.time = new Date().toUTCString();
+    req.user = req.ip;
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time, user: req.user });
+  }
+);
 
-
- /* Serve JSON on a route */
- app
-    .route('/now')
-    .get( (req, res) => {
-        const time = new Date().toUTCString();
-        res.json({"Current Time": time})
-    })
-
-
-app
-    .route('/hello')
-    .get( (req, res) => {
-        const msg = process.env.MESSAGE_STYLE === 'uppercase' ? 'JSON' : 'json';
-        res.json( {"message" : `Hello ${msg}`})
-    })
-
-
-
-
-
+app.route("/hello").get((req, res) => {
+  const msg = process.env.MESSAGE_STYLE === "uppercase" ? "JSON" : "json";
+  res.json({ message: `Hello ${msg}` });
+});
