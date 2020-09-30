52cb7bbf7ebc81d337b7ced60ad32b7d5fa912ca
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-30 22:17:31 +0300

    format code

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-"use strict";
-console.clear();
-global.log = console.log;
+'use strict'
+console.clear()
+global.log = console.log
 
-const express = require("express");
+const express = require('express')
 
-const { 
-    notFound, 
-    convertAPI, 
-    nowAPI, 
-    home 
-} = require("./routes/routes");
+const { notFound, convertAPI, nowAPI, home } = require('./routes/routes')
 
+const { PORT } = require('./evn_vars')
 
-const {
-  PORT
-} = require('./evn_vars');
+const app = express()
+app.listen(3000, log(`Server running on port ${PORT} \n`))
 
-
-
-const app = express();
-app.listen(3000, log(`Server running on port ${PORT} \n`));
-
-
-const statics = express.static(process.cwd() + "/public");
-app.use(statics);
-
+const statics = express.static(process.cwd() + '/public')
+app.use(statics)
 
 // ROUETES
 home(app)
 nowAPI(app)
-convertAPI(app);
+convertAPI(app)
 notFound(app)
